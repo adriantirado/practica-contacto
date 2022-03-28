37c185b5f871444ce6bc89924bebb77a13d3f678
adrian <83710698+adriantirado@users.noreply.github.com>
2022-03-28 19:50:39 +0200

    Add files via upload

--- src/components/pure/forms/contactos.jsx
+++ src/components/pure/forms/contactos.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { contact } from '../../../models/contact.class';
-import Contactosb from './contactosb';
+import '../../../styles/contacto.css' ;
 
 
-const Contactos = ({contact}) => {
 
+const Contactos = ({ contact,complete, remove}) => {
+    function contactoCompletedIcon(){
+        if(contact.conectado){
+            return   (<i onClick={()=>complete(contact)} className='bi bi-telephone concat-estado' style={{ color: 'green' }}></i>)
+        }else{
+            return (<i onClick={()=>complete(contact)} className='bi bi-telephone-x concat-estado' style={{ color: 'grey' }}></i>)
+        }
+    }
     return (
-        <div>
-           <h1>Nombre: {contact.name}</h1> 
-           <h2>Apellido: {contact.apellido}</h2>
-           <h3>email: {contact.email}</h3>
-           <Contactosb conectado={false}></Contactosb>
-        </div>
+        <tr className='fw-normal'>
+            <th>
+                <span> {contact.name}</span>
+            </th>
+            <td className='align-middle'>
+                <span>{contact.apellido}</span>
+            </td>
+            <td className='align-middle'>
+                <span>{contact.email}</span>
+            </td>
+            <td className='align-middle'>
+
+              {contactoCompletedIcon()}
+                <i className='bi-trash task-action concat-estado ' style={{color: 'tomato'}} onClick={() => remove(contact)}></i>
+            </td>
+        </tr>
+
+
+
+
+
+        
+
     );
 };
 
 
 Contactos.propTypes = {
-    contact:PropTypes.instanceOf(contact)
+    contact: PropTypes.instanceOf(contact).isRequired,
+    complete:PropTypes.func.isRequired,
+    remove:PropTypes.func.isRequired
 };
 
 
