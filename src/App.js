05769d210b3e4b7e081763398784dd41d2b0fc83
adrian <adriancrack123@gmail.com>
2022-03-30 18:02:37 +0200

    segundo

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Relox2 from './components/pure/relox2';
 import Reloj from './components/pure/forms/Reloj';
 import Contactlist from './components/containers/contactlist';
+import Mouse from './components/pure/forms/Mouse';
 
 function App() {
   return (
     <div className="App">
  
-    <Contactlist></Contactlist>
-       
+   {/*<Contactlist></Contactlist>*/}
+      <Mouse></Mouse>
      
     </div>
   );
